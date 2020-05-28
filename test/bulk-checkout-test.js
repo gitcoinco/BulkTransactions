c6e1d28310c59b42afac88c908e4b6c5246c401f
mds <17163988+mds1@users.noreply.github.com>
2020-05-28 13:26:00 -0700

    Setup linting

--- test/bulk-checkout-test.js
+++ test/bulk-checkout-test.js
@@ -5,14 +5,14 @@ const { expect } = require('chai');
 const BulkCheckout = contract.fromArtifact('BulkCheckout');
 
 describe('BulkCheckout', () => {
-    const [ owner ] = accounts;
+  const [owner] = accounts;
 
-    before(async () => {
-        this.bulkCheckout = await BulkCheckout.new({ from: owner });
-      });
+  before(async () => {
+    this.bulkCheckout = await BulkCheckout.new({ from: owner });
+  });
 
-      it('should see the deployed BulkCheckout contract', async () => {
-        expect(this.bulkCheckout.address.startsWith('0x')).to.be.true;
-        expect(this.bulkCheckout.address.length).to.equal(42);
-      });
+  it('should see the deployed BulkCheckout contract', async () => {
+    expect(this.bulkCheckout.address.startsWith('0x')).to.be.true;
+    expect(this.bulkCheckout.address.length).to.equal(42);
+  });
 });
