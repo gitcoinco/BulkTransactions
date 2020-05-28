e377161fd88e25554d28b39cb53bf04e4f7601ce
mds <17163988+mds1@users.noreply.github.com>
2020-05-28 13:26:00 -0700

    Setup linting

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
+  // This option interrupts the configuration hierarchy at this file
+  // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
+  root: true,
+
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+
+  env: {
+    browser: true,
+    es6: true,
+    mocha: true,
+  },
+
+  // Rules order is important, please avoid shuffling them
+  extends: [
+    // Base ESLint recommended rules
+    // 'eslint:recommended',
+    'airbnb-base',
+    'plugin:chai-friendly/recommended',
+  ],
+
+  plugins: [
+  ],
+
+  globals: {
+    ga: true, // Google Analytics
+    cordova: true,
+    __statics: true,
+    process: true,
+    Capacitor: true,
+    chrome: true,
+  },
+
+  // add your custom rules here
+  rules: {
+    'no-param-reassign': 'off',
+
+    'import/first': 'off',
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'prefer-promise-reject-errors': 'off',
+
+
+    // allow debugger during development only
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+     // Max line length of 100 characters, except for lines with comments or strings
+     'max-len': ['error', {
+      code: 100, ignoreComments: true, ignoreTrailingComments: true, ignoreStrings: true,
+    }],
+  },
+};
