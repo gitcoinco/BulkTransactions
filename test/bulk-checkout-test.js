6a81b84c73f31392a9375bfa7b0ab8947e9c0c63
mds <17163988+mds1@users.noreply.github.com>
2020-05-28 13:57:26 -0700

    First draft of contract with unimplemented tests. No ETH support

--- test/bulk-checkout-test.js
+++ test/bulk-checkout-test.js
@@ -11,8 +11,29 @@ describe('BulkCheckout', () => {
     this.bulkCheckout = await BulkCheckout.new({ from: owner });
   });
 
+
   it('should see the deployed BulkCheckout contract', async () => {
     expect(this.bulkCheckout.address.startsWith('0x')).to.be.true;
     expect(this.bulkCheckout.address.length).to.equal(42);
+  });
+
+  it('lets the user submit only one donation for a token', async () => {
+    expect(true).to.equal(false);
+  });
+
+  it('lets the user submit only one donation of ETH', async () => {
+    expect(true).to.equal(false);
+  });
+
+  it('lets the user submit multiple donations of only tokens', async () => {
+    expect(true).to.equal(false);
+  });
+
+  it('lets the user submit multiple donations of only ETH', async () => {
+    expect(true).to.equal(false);
+  });
+
+  it('lets the user submit multiple donations as mix of tokens and ETH', async () => {
+    expect(true).to.equal(false);
   });
 });
